fd99861a6d6b4b2381e9b8dae5e64f62b56675a5
ck <dbcjdrb1997@gmail.com>
2022-03-26 23:07:15 +0900

    Kotlin #31 - finished all success of course
    regret
    : 제너릭
    : 고차함수
    : 의존성주입
    : inner

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
+class Rectangle(var height: Double, var length: Double){
+    var perimeter = (height + length) * 2
+}
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
+    val rectangle = Rectangle(5.0,2.0)
+    println("${rectangle.perimeter}")
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
