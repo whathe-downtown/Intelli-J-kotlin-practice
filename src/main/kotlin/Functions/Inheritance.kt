03bf31f7511b4b7e66e864e1567329a70f3d7108
ck <dbcjdrb1997@gmail.com>
2022-03-30 23:52:59 +0900

    Kotlin #33 - Null Safety - Generics - Inheritance

---
+++ src/main/kotlin/Functions/Inheritance.kt
@@ -0,0 +1,18 @@
+package Functions
+
+open class Dog {
+    open fun bark(){
+        println("WOW WOW")
+    }
+}
+
+class Ruby: Dog(){
+    override fun bark() {
+        println("멈ㅇ 멍")
+    }
+}
+
+fun main(){
+    val dog: Dog = Ruby()
+    dog.bark()
+}
