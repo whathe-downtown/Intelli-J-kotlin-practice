e7ee4effa8131abec8c9f28280a9e3ac5f410ecb
ck <dbcjdrb1997@gmail.com>
2022-03-30 23:32:08 +0900

    Kotlin #33 - Null Safety - Generics - Inheritance

---
+++ src/main/kotlin/Functions/Generics.kt
@@ -0,0 +1,7 @@
+package Functions
+
+
+
+
+fun main() {
+}
---
+++ src/main/kotlin/Functions/NullSafety.kt
@@ -0,0 +1,15 @@
+package Functions
+
+fun main() {
+
+
+    println(describeStr9ng(null))
+}
+
+fun describeStr9ng(maybeString: String?): String{
+    if (maybeString != null && maybeString.length >0){
+        return  "String of length ${maybeString.length}"
+    } else{
+        return "Empty or null String"
+    }
+}
